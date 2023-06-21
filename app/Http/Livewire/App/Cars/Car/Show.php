47ee731428ddb2f84f1aa94bc3bf53c2367f4ad1
LaraGEN <laragen@hehecoding.com>
2023-06-21 08:20:51 +0000

    Commit #73 made by LaraGEN @2023-06-21 08:20:51

---
+++ app/Http/Livewire/App/Cars/Car/Show.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Livewire\App\Cars\Car;
+
+use Livewire\Component;
+use App\Http\Livewire\Generated\Cars\Car\ShowTrait;
+
+class Show extends Component
+{
+    use ShowTrait;
+}
